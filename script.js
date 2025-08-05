60d89c319f634e861c969d014cd34876ccccfe47
nh <nickxhan@umich.edu>
2025-08-05 01:07:44 -0400

    Change approach to arrays
    About to stop using operations as the way to track
    steps.

--- script.js
+++ script.js
@@ -9,37 +9,63 @@ const numbers = document.querySelector(".numbers");
 const operations = document.querySelector(".operations");
 const final = document.querySelector(".bottom");
 
-function beginCalculation() {
+let numList = [];
+let opList = [];
+let ongoing = false;
 
+function clear() {
+    numList = [];
+    opList = [];
+    ongoing = false;
+    input.textContent = "";
 }
 
-let first = "";
-let second = "";
-let operation = "";
-// input.addEventListener("change", () => beginCalculation());
-
 numbers.addEventListener("click", (e) => {
-    let num = e.target.textContent;
-    if (num == '.')
-    {
-        
+    if (!ongoing) {
+        clear();
+        ongoing = true;
     }
-    if (Number.isInteger(Number(num)))
-    {
+    let num = e.target.textContent;
+    if (num == '.') {
+
+    }
+    if (Number.isInteger(Number(num))) {
         if (num === "0" && input.textContent.length == 0) {
             return;
         }
         input.textContent += num;
-        // b = input.textContent;
     }
-        
+
 })
 
+//this determines when accumulated/current are set; splits up things
 operations.addEventListener("click", (e) => {
-    let operation = e.target.textContent;
-    let a = input.textContent;
-    if (a.length > 0) {
-        first = a;
-        input.textContent = "";
+    let currentOperation = e.target.textContent;
+    let text = input.textContent;
+    //if one of the four operations is clicked
+    if (text.length > 0 && currentOperation.length == 1) {
+        numList.push(text);
+        //if this isn't the first operation press
+        if (numList.length !== 0) {
+            let index = (opList.length - 1) * 2;
+            let previousResult = operate(numList[index], numList[index + 1], opList.at(-1));
+            input.textContent = previousResult;
+            numList.push(previousResult);
+        }
+        //push after b/c do not need this yet
+        //if statement to deal w/ consecutive operation presses
+        opList.push(currentOperation);
+        current = text;
+    }
+})
+
+final.addEventListener("click", (e) => {
+    ongoing = false;
+    let action = e.target.textContent;
+    if (action === "=") {
+        input.textContent = operate(accumulated, current, operation);
+    }
+    else if (action == "CLEAR") {
+        clear();
     }
 })
